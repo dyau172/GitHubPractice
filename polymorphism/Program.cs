f77e00f37cb458b9af89b16c666c6277b69ba827
dyau172 <47676379+dyau172@users.noreply.github.com>
2020-10-22 09:56:47 +0100

    Created int addition method

---
+++ polymorphism/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int addedSumOfInts = AddTwoInt(7,13);
+            Console.WriteLine("Int: " + addedSumOfInts);
+        }
+
+        static int AddTwoInt(int x, int y)
+        {
+            return x + y;
+
+
+        }
+    }
+}
