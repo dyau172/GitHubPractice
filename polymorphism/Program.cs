fd2aab52a5b90e58fbae5ba6c8a489e33161573f
dyau172 <47676379+dyau172@users.noreply.github.com>
2020-10-22 10:06:54 +0100

    Replaced addition methods with polymor versions

--- polymorphism/Program.cs
+++ polymorphism/Program.cs
@@ -3,19 +3,22 @@
 namespace polymorphism {
     class Program {
         static void Main (string[] args) {
-            int addedSumOfInts = AddTwoInt (7, 13);
+            int addedSumOfInts = AddTwoNumbers (7, 13);
 
-            int addedSumOfDoubles = AddTwoDoubles (7.5, 13.8)
+            double addedSumOfDoubles = AddTwoNumbers (7.5, 13.8);
+
             Console.WriteLine ("Int: " + addedSumOfInts);
+            Console.WriteLine ("Double: " + addedSumOfDoubles);
         }
 
-        static int AddTwoInt (int x, int y) {
+        static int AddTwoNumbers (int x, int y) {
             return x + y;
 
         }
 
-        static double AddTwoDouble (double x, double y) {
+        static double AddTwoNumbers (double x, double y) {
             return x + y;
         }
+
     }
 }
